1e962fccf24ca1f8c229ac7b2c734ca30c62c250
AksharDesai <akshar25desai@gmail,com>
2025-02-24 16:14:31 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState , useEffect} from 'react'
+
+function App() {
+
+
+  return (
+    <>
+  
+  <h1 className='text-white '>Hello Ladies</h1>
+
+    </>
+  )
+}
+
+export default App
